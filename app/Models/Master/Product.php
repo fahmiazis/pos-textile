50d220c147f6e55676cc0c0b4e1e3410ce4c0e4d
IT-SAS <ihsanalvindra@gmail.com>
2026-01-12 23:27:31 +0700

    Make Migration Product, Sales Pricing, Purchase Pricing And Make Model Product, Sales Pricing, Purchase Pricing

---
+++ app/Models/Master/Product.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models\Master;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'sku',
+        'name',
+        'brand_id',
+        'category_id',
+        'base_uom_id',
+        'description',
+        'is_active',
+    ];
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function baseUnit()
+    {
+        return $this->belongsTo(Unit::class, 'base_uom_id');
+    }
+}
