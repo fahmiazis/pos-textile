e274deb6ee4292143dafcd28aea026a11d56884b
IT-SAS <ihsanalvindra@gmail.com>
2026-01-12 23:31:07 +0700

    Make Migration Vehicle  & Make Model Vehicle Table

---
+++ database/migrations/2026_01_12_162949_create_vehicles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('plate_number', 15)->unique();   // B 1234 CD
+            $table->enum('vehicle_type', ['TRUCK', 'PICKUP']);
+            $table->decimal('capacity_meter', 12, 3)->nullable(); // kapasitas meter kain
+
+            $table->boolean('is_active')->default(true);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vehicles');
+    }
+};
