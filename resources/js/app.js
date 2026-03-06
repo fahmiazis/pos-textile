2a155a213721d7a9995c81c2ddba70cf074eb1b2
fahmiazis <fahmiazis797@gmail.com>
2026-03-07 06:25:21 +0700

    modified:   resources/js/app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -17,8 +17,8 @@ createInertiaApp({
     title: () => `${appName}`,
     resolve: (name) =>
         resolvePageComponent(
-            `./Pages/${name}.vue`,
-            import.meta.glob('./Pages/**/*.vue'),
+            `./pages/${name}.vue`,
+            import.meta.glob('./pages/**/*.vue'),
         ),
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
