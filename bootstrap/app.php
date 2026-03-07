6eadd9445878f288211e3501cea7fb66043351bd
fahmiazis <fahmiazis797@gmail.com>
2026-03-07 07:17:38 +0700

    modified:   app/Providers/AppServiceProvider.php 	modified:   bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -95,5 +95,9 @@ return Application::configure(basePath: dirname(__DIR__))
         });
     })
 
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+    })
+
 
     ->create();
